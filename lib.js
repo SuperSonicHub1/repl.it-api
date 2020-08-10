61c899d0d5858b61ff269e23ec063d74d993af2c
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2020-08-10 14:37:06 -0400

    Resolved JSON parsing issues.
    The access token is now plain text, which caused the loadFromPath function to break, so I just changed the if statement at the end to "this.got.token = potentialToken.response".

--- lib.js
+++ lib.js
@@ -64,16 +64,8 @@ module.exports = class {
         polygott: false
       }),
       headers
-    }).then(parseJson)
-    if (potentialToken.message) {
-      if (potentialToken.name) {
-        throw new Error(`${potentialToken.name}: ${potentialToken.message}`)
-      } else {
-        throw new Error(potentialToken.message)
-      }
-    } else {
-      this.got.token = potentialToken
-    }
+    });
+    this.got.token = potentialToken.response;
   }
 
   login(sid) {
